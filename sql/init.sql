a49af38e4c5eeb3165d34d30bbfa4fdc318aab3e
wangliang <1991wangliang@gmail.com>
2019-07-27 14:36:28 +0800

    init sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS test;
 
-CREATE USER 'prod'@'localhost' IDENTIFIED BY 'prod';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'prod'@'localhost';
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 
 
 USE `test` ;
