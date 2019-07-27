4bbe65c03895b74dcd61f3b9d0c13b8c78e24a43
wangliang <1991wangliang@gmail.com>
2019-07-27 10:44:46 +0800

    init sql

--- sql/init.sql
+++ sql/init.sql
@@ -7,8 +7,8 @@ GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'prod'@'localhost';
 USE `test` ;
 
 
-DROP TABLE IF EXISTS `t_test`;
-CREATE TABLE `t_test` (
+DROP TABLE IF EXISTS `t_demo`;
+CREATE TABLE `t_demo` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
