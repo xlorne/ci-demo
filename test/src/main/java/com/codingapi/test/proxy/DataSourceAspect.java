fd89b47dd9f5af71fc7ed8035cfbd7b336e9ff22
wangliang <1991wangliang@gmail.com>
2019-08-04 21:14:42 +0800

    proxy

---
+++ test/src/main/java/com/codingapi/test/proxy/DataSourceAspect.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017-2019 CodingApi .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codingapi.test.proxy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * create by lorne on 2018/1/5
+ * @author lorne
+ */
+@Aspect
+@Component
+@Slf4j
+public class DataSourceAspect {
+
+
+    @Around("execution(* javax.sql.DataSource.getConnection(..))")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        log.info("around----1");
+        Object res = point.proceed();
+        TestContext testContext = new TestContext();
+        testContext.setResource(res);
+        log.info("around----2");
+        return res;
+    }
+
+
+}
