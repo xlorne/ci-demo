3731ca612aee72ffb3c8127fc064a0200f858b5a
wangliang <1991wangliang@gmail.com>
2019-08-07 13:51:02 +0800

    remove vo

--- src/main/java/com/codingapi/cidemo/controller/DemoController.java
+++ src/main/java/com/codingapi/cidemo/controller/DemoController.java
@@ -1,16 +1,10 @@
 package com.codingapi.cidemo.controller;
 
-import com.codingapi.cidemo.domain.Demo;
 import com.codingapi.cidemo.service.DemoService;
-import com.codingapi.cidemo.vo.LoginReq;
-import com.codingapi.cidemo.vo.LoginRes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author lorne
@@ -24,18 +18,10 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
-    @PostMapping("/save")
-    public boolean save(@RequestBody Demo demo){
-        return demoService.save(demo);
-    }
-
     @PostMapping("/login")
-    public LoginRes login(@RequestBody LoginReq loginReq) {
-        return demoService.login(loginReq);
+    public Long login(@RequestBody String name) {
+        return demoService.login(name);
     }
 
-    @GetMapping("/list")
-    public List<Demo> list(){
-        return demoService.list();
-    }
+
 }
